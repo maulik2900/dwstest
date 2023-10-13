1f07b2a25013fc357e9c7b6aec50b8d826899c13
maulik-paneliya <maulikpaneliyadws@gmail.com>
2023-10-13 15:26:01 +0530

    my first commit

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+echo 'new';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+</head>
+<body>
+
+<h1>Hello world!</h1>
+<p>This is tt the first file in my new Git Repo.</p>
+
+</body>
+</html>
+
+
